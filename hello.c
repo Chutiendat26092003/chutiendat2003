61202b2a64f2921b6eb3d7df006a25870931d018
Chutiendat26092003 <91305197+Chutiendat26092003@users.noreply.github.com>
2021-09-26 08:45:01 +0700

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a = 1;
+	int b = 5;
+	int c;
+	 c = a + b;
+	printf("hello dat\n");
+	printf("c = %d\n", c);
+	printf("%d + %d = %d", a,b,c);
+	return 0;
+}
