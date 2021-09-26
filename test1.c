0419a5af223042dec311409abf71d5ac10a0e5dd
Chutiendat26092003 <91305197+Chutiendat26092003@users.noreply.github.com>
2021-09-26 09:09:57 +0700

    Add files via upload

---
+++ test1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("e chao co\n");
+	printf("e moi tim hieu ve cntt nen e hoi cham\n");
+	printf("mong co thong cam giup e nha\n");
+	
+	return 0;
+}
